8a952a5b2058cae4081ca33897a01aa58a74ac1c
gracehg <grace@statusforward.com>
2020-10-26 17:32:05 -0400

    finished widgets

---
+++ src/components/Route.js
@@ -0,0 +1,19 @@
+// Don't need to import React if not writing JSX
+import { useEffect, useState } from 'react';
+
+const Route = ({ path, children }) => {
+      const [currentPath, setCurrentPath] = useState(window.location.pathname);
+      useEffect(() => {
+            const onLocationChange = () => {
+                  // console.log('Location Change');
+                  setCurrentPath(window.location.pathname);
+            }
+            window.addEventListener('popstate', onLocationChange);
+            return () => {
+                  window.removeEventListener('popstate', onLocationChange);
+            }
+      }, []);
+      return currentPath === path ? children : null;
+};
+
+export default Route;
--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Convert';
 
 const options = [
       {
@@ -33,6 +34,9 @@ const Translate = () => {
                         selected={language}
                         onSelectedChange={setLanguage}
                   />
+                  <hr />
+                  <h3 className="ui header">Output</h3>
+                  <Convert text={text} language={language} />
             </div>
       );
 }
